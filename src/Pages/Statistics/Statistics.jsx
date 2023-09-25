e23a84cf2e70da6ad68f4e86383491ef8362bbe2
Bipon Roy <biponroy400@gmail.com>
2023-09-25 23:30:16 +0600

    Added Pie Chart functionality on Statistics Components

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,7 +1,39 @@
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from "recharts";
+import { getStorageData } from "../../utility/localStorage";
+import { useLoaderData } from "react-router-dom";
+
 const Statistics = () => {
+    const totalDonation = getStorageData();
+    const allData = useLoaderData();
+    const totalValue = allData.length;
+
+    const donatedValue = totalDonation.length;
+
+    const data = [
+        { name: "Your Donation", value: donatedValue },
+        { name: "Total Donation", value: totalValue - donatedValue },
+    ];
+
+    const COLORS = ["#00C49F", "#FF444A"];
+
     return (
-        <div>
-            <h1>Statistics</h1>
+        <div className="max-w-7xl mx-auto">
+            <div>
+                <h2>Pie Chart</h2>
+                <ResponsiveContainer width="100%" height={500}>
+                    <PieChart>
+                        <Pie dataKey="value" data={data} cx="50%" cy="50%" fill="#8884d8">
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Tooltip
+                            formatter={(value) => `${((value / totalValue) * 100).toFixed(2)}%`}
+                        />
+                        <Legend />
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -32,6 +32,7 @@ const router = createBrowserRouter([
             {
                 path: "/statistics",
                 element: <Statistics />,
+                loader: () => fetch("/DonationData.json"),
             },
         ],
     },
