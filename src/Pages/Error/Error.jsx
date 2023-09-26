50127e12e541e4f567a79bb0087c783ac6bd79b9
Bipon Roy <biponroy400@gmail.com>
2023-09-26 23:19:10 +0600

    updated error page design

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -6,9 +6,9 @@ const Error = () => {
         <div className="max-w-7xl mx-auto">
             <Navbar />
             <div className="flex flex-col justify-center items-center h-[70vh]">
-                <h1 className="text-3xl font-semibold">
-                    <span className="text-4xl text-[#FF444A]">Oops</span> Nothing Founds In this
-                    Page!!!
+                <h1 className="text-[4rem] text-[#FF444A] mb-3">404!</h1>
+                <h1 className="text-3xl font-semibold text-center">
+                    Nothing Founds In this Page!!!
                 </h1>
                 <Link to="/">
                     <button className="px-7 py-3 text-white font-medium bg-[#FF444A] mt-6 rounded-md">
